52669a80cbed09af0978dce0a674c89b5fdfd2f8
admiral-f <admiral-f@yandex.ru>
2015-12-09 20:37:17 +0300

    small changes at articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
-
-  end
-
-  def create
     @article=Article.new(article_params)
     if @article.valid?
       @article.save
