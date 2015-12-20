1d433105f5173b9046ab47018056573917c49520
admiral-f <admiral-f@yandex.ru>
2015-12-20 21:40:05 +0300

    update visitor create spec

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -7,14 +7,6 @@ feature "Account Creation" do
   end
 end
 
-def sign_up
-    visit new_user_registration_path
-    fill_in :user_email, :with => 'user@example.com'
-    fill_in :user_username, :with => 'Alex'
-    fill_in :user_password, :with => '123456789'
-    fill_in :user_password_confirmation, :with => '123456789'
 
-    click_button 'Sign up'
-end
 
  
