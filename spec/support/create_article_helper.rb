76cbd453ff7bcd586249083281375932dc067d0d
admiral-f <admiral-f@yandex.ru>
2015-12-21 14:39:05 +0300

    update create article helper

--- spec/support/create_article_helper.rb
+++ spec/support/create_article_helper.rb
@@ -3,5 +3,4 @@ def create_article
     fill_in :article_title, :with => 'example title'
     fill_in :article_text, :with => 'example text'
     click_button 'Yarrr!'
-    expect(page).to have_content I18n.t('articles.article_comments')
 end
