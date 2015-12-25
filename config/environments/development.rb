92f90905c08d326aaab884b94991a60f725f74e9
admiral-f <admiral-f@yandex.ru>
2015-12-26 01:32:22 +0300

    add avatar image to user table

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Paperclip.options[:command_path] = 'C:\ImageMagick-6.9.2-Q16'
+
 end
