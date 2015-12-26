19d5a5b266e15d39c7fee2943d54b97616184beb
admiral-f <admiral-f@yandex.ru>
2015-12-26 11:45:19 +0300

    add avatar column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225192849) do
+ActiveRecord::Schema.define(version: 20151226081320) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151225192849) do
     t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar_url"
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
